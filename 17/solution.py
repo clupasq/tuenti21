1b166413a0db422b03c63d3e1d0c039e403a9562
Cristian Lupascu <cristi.lupascu@pm.me>
2021-12-20 01:01:17 +0200

    Solve #17. Or not???

--- 17/solution.py
+++ 17/solution.py
@@ -2,6 +2,7 @@
 from math import log, floor
 from rich import print
 from itertools import product
+from collections import Counter
 
 def read_int():
     return int(input())
@@ -46,7 +47,7 @@ def is_winning_situation(heaps):
     if len(heaps) == 0:
         return False
     if len(heaps) == 1:
-        return heaps[1] % 3 > 0
+        return heaps[0] % 3 > 0
     for i, h in enumerate(heaps):
         for p2 in range(0, floor(log(h, 2)) + 1):
             l = list(heaps)
@@ -68,22 +69,93 @@ def is_winning_situation(heaps):
     return False
 
 def simulate_multiple_heaps():
-    for l in range(2, 6):
-        for heaps in product([list(range(7))] * l):
-            print(heaps)
 
-simulate_multiple_heaps()
+    dim = 7
+    k = 1
+    l = 2
+    m = 3
+    n = 3
+    print("  ", end="")
+    for j in range(1, dim):
+        print(f"  {j:2}  ", end="")
+    print()
+    for i in range(1, dim):
 
-def solution():
-    # code solution here
-    pass
+        print(f"{i:2} ", end="")
+        for j in range(1, dim):
+            result = is_winning_situation((i, j, k))
+            result = "False " if not result else "True  "
+            print(result, end="")
+        print()
+
+    # for l in range(2, 4):
+    #     for heaps in product(*[list(range(1, 7))] * l):
+    #         print(heaps, is_winning_situation(heaps))
+
+# simulate_multiple_heaps()
+
+# for i in [1,2,3]:
+#     for j in range(1, 7):
+#         t = tuple([i] * j)
+#         print(t, is_winning_situation(t))
+
+def other_random_tests():
+    for t in [
+            (1, 1, 3),
+            (2, 2, 3),
+
+            (1, 1, 2, 2, 3, 3, 3, 3),
+
+            (1, 2),
+            (1, 1, 1, 1, 2, 2, 2, 2, 2, 2),
+            (1, 1, 2, 2, 3, 3),
+            (1, 1, 1, 2, 2, 2, 3, 3, 3),
+            # (1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3),
+
+            (1, 1, 1, 1, 2, 2, 3, 3),
+
+            (1, 1, 1),
+            ]:
+        print(t, is_winning_situation(t))
+        print()
+
+# Conclusion:
+#   only the values %3 matter; no need to work with huge numbers.
+#   WOW: heaps with %3==0 can be reoved and the result does not change!
+#   OK, it seems that if the counts of ones and twos are *both* even, it's a losing situation.
+#   Otherwise, it's a win
+
+
+def mod3(x):
+    """
+    Avoid working with 0s; it messes with the log function
+    """
+    m = x % 3
+    return 3 if m == 0 else m
+
+def solution_old(xs):
+    """
+    This one passed the test input
+    """
+    xs = [mod3(x) for x in xs]
+    is_winning = is_winning_situation(tuple(xs))
+    return "Edu" if is_winning else "Alberto"
+
+def solution(xs):
+    xc = Counter([mod3(x) for x in xs])
+    ones = xc[1]
+    twos = xc[2]
+    is_winning = ones % 2 > 0 or twos % 2 > 0
+    return "Edu" if is_winning else "Alberto"
 
 def main():
     t = read_int()
 
     for i in range(t):
-        s = solution()
+        n = read_int()
+        xs = read_int_array()
+        s = solution(xs)
         print(f"Case #{ i + 1 }: {s}")
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
